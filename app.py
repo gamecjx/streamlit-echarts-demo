e256d9a9c0f5ce4f6bd98d619726dfe48e5042fc
gamecjx <54061279+gamecjx@users.noreply.github.com>
2024-03-18 17:11:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,14 +12,19 @@ def main():
 
     with st.sidebar:
         st.header("选择算法与应用场景")
-        api_options = ("MSDBO", "DBO","SSA","BWO","GWO","PO","KOA","WOA")
+        api_options = ("echarts", "pyecharts")
         selected_api = st.selectbox(
-            label="请选择算法:",
+            label="Choose your preferred API:",
             options=api_options,
         )
-        page_options = ("CEC2017", "CEC2021")
+
+        page_options = (
+            list(ST_PY_DEMOS.keys())
+            if selected_api == "pyecharts"
+            else list(ST_DEMOS.keys())
+        )
         selected_page = st.selectbox(
-            label="请选择应用场景",
+            label="Choose an example",
             options=page_options,
         )
         demo, url = (
@@ -27,22 +32,40 @@ def main():
             if selected_api == "echarts"
             else ST_PY_DEMOS[selected_page]
         )
-    image_path = get_image_path(selected_api, selected_page)
-    st.image(image_path)
 
-def get_image_path(selected_api, selected_page):
-    # 这里是一个示例，您需要根据实际情况来设置图片路径
-    if selected_api == 'MSDBO' and selected_page == 'CEC2017':
-        return 'data/26.jpg'
-    elif selected_api == 'DBO' and selected_page == 'CEC2021':
-        return 'data/27.jpg'
-    # 添加更多条件以覆盖所有选择组合
-    else:
-        return 'data/26.jpg'
+        if selected_api == "echarts":
+            st.caption(
+                """ECharts demos are extracted from https://echarts.apache.org/examples/en/index.html, 
+            by copying/formattting the 'option' json object into st_echarts.
+            Definitely check the echarts example page, convert the JSON specs to Python Dicts and you should get a nice viz."""
+            )
+        if selected_api == "pyecharts":
+            st.caption(
+                """Pyecharts demos are extracted from https://github.com/pyecharts/pyecharts-gallery,
+            by copying the pyecharts object into st_pyecharts. 
+            Pyecharts is still using ECharts 4 underneath, which is why the theming between st_echarts and st_pyecharts is different."""
+            )
 
-    
+    demo()
+
+    sourcelines, _ = inspect.getsourcelines(demo)
+    with st.expander("Source Code"):
+        st.code(textwrap.dedent("".join(sourcelines[1:])))
+    st.markdown(f"Credit: {url}")
+
+
 if __name__ == "__main__":
     st.set_page_config(
         page_title="Streamlit ECharts Demo", page_icon=":chart_with_upwards_trend:"
     )
     main()
+    with st.sidebar:
+        st.markdown("---")
+        st.markdown(
+            '<h6>Made in &nbsp<img src="https://streamlit.io/images/brand/streamlit-mark-color.png" alt="Streamlit logo" height="16">&nbsp by <a href="https://twitter.com/andfanilo">@andfanilo</a></h6>',
+            unsafe_allow_html=True,
+        )
+        st.markdown(
+            '<div style="margin-top: 0.75em;"><a href="https://www.buymeacoffee.com/andfanilo" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" height="41" width="174"></a></div>',
+            unsafe_allow_html=True,
+        )
